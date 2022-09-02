9c364cfddeb6d845af91a8131e073755a849c4cb
Pongpan Ingpanuwong <pingpanu@z3t1c4.42bangkok.com>
2022-09-02 14:52:52 +0700

    remove main

--- srcs/r_rotate.c
+++ srcs/r_rotate.c
@@ -6,7 +6,7 @@
 /*   By: pingpanu <pingpanu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 14:09:54 by pingpanu          #+#    #+#             */
-/*   Updated: 2022/09/02 14:51:17 by pingpanu         ###   ########.fr       */
+/*   Updated: 2022/09/02 14:52:34 by pingpanu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,37 +41,4 @@ void	r_rotate_ab(t_stack **a, t_stack **b)
 	r_rotate_stack(a);
 	r_rotate_stack(b);
 	ft_putendl_fd("rrr", 1);
-}
-
-#include <stdio.h>
-static void     print_lst(t_stack *lst)
-{
-    if (lst == NULL)
-        printf("Stack is empty");
-    while (lst != NULL)
-    {
-        printf("%d-> ",lst->data);
-        lst = lst->next;
-    }
-    printf("\n");
-}
-
-int     main()
-{
-    int     arr[5] = {3, 1, 5, 2, 4};
-    t_stack     *new;
-    t_stack     *a;
-
-    a = NULL;
-    int     i = 0;
-    while (i < 5)
-    {
-        new = ft_lstnew(arr[i]);
-        ft_lstadd_back(&a, new);
-        i++;
-    }
-    print_lst(a);
-    r_rotate_a(&a);
-    print_lst(a);
-    return (0);
 }
